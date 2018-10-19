e2164a545d30466387eb6a127e1039e100391f14
sg <stuart.a.gill@gmail.com>
2018-10-19 13:20:25 -0700

    Add back end branching logic functions in JS. All paths are now called by various combinations of answers, but not yet passed on to front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,47 @@
 $(document).ready(function() {
   $('#quiz').submit(function(event) {
     event.preventDefault();
+    var person1 = $("input#person1").val();
     var income = $("input:radio[name=income]:checked").val();
     var companyType = $("input:radio[name=companyType]:checked").val();
     var work = $("input:radio[name=work]:checked").val();
     var android = $("input:radio[name=android]:checked").val();
     var computer = $("input:radio[name=computer]:checked").val();
     var qArray = [income, companyType, work, android, computer];
-
+    alert(person1);
 
     if (qArray.includes("badFit")) {
       alert("Bad Fit");
     }
-
-    if (income === 'yes') {
-      alert("yes");
+    else if (android === 'no' || android === 'maybe'){
+      if (companyType === 'government'){
+        alert("PHP");
+      }
+      else if (companyType === 'startup'){
+        alert("Ruby");
+      }
+      else if (companyType === 'established'){
+        alert("C#");
+      }
+      else if (work === 'marketing'){
+        alert("PHP");
+      }
+      else if (work === 'web'){
+        alert("Ruby")
+      }
+      else {
+        alert("Java");
+      }
     }
-    else if (gender == 'female') {
-      $("img").hide();
-      $("#tyrion").show();
+    else if (companyType === 'enterprise' || work === 'backend' || android === 'yes') {
+      alert("Java");
     }
     else {
-      $("img").hide();
-      $("#meli").show();
+      alert("C#");
     }
+    // else if (companyType === 'startup') {
+      // $("img").hide();
+      // $("#tyrion").show();
+    // }
   });
 });
