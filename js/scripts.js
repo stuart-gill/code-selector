aa6e7bf410daec33df098a62776780c8e47b5f33
sg <stuart.a.gill@gmail.com>
2018-10-19 11:13:48 -0700

    Add first three questions in html and coordinating variables in JS

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
     event.preventDefault();
     var age = $("#age").val();
     var gender = $("#gender").val();
-    var rpg = $("input:radio[name=operator]:checked").val();
+    var companyType = $("input:radio[name=companyType]:checked").val();
+    var work = $("input:radio[name=work]:checked").val();
+
 
     if (age > 40 && rpg == 'palladin') {
       $("img").hide();
