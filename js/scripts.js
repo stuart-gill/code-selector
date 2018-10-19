bd9f4ff8204da74ad1902e8eef07ba4018600a0d
sg <stuart.a.gill@gmail.com>
2018-10-19 11:38:06 -0700

    Add two more questions and options in html, and clean up indentation and white space. Add breaks for legibility

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $('#quiz').submit(function(event) {
     event.preventDefault();
-    var age = $("#age").val();
-    var gender = $("#gender").val();
+    var income = $("input:radio[name=income]:checked").val();
     var companyType = $("input:radio[name=companyType]:checked").val();
     var work = $("input:radio[name=work]:checked").val();
+    var android = $("input:radio[name=android]:checked").val();
 
 
     if (age > 40 && rpg == 'palladin') {
